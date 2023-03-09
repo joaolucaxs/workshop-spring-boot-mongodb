e02101d97e2f471cd9bd95ac59e4275e72a21b65
João Lucas <joo.lucas994@gmail.com>
2023-03-09 17:56:45 -0300

    Obtendo usuário por id + Exceptions Service and Resources

---
+++ src/main/java/com/joaolucas/workshopmongodb/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.joaolucas.workshopmongodb.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.joaolucas.workshopmongodb.services.exceptions.DataBaseException;
+import com.joaolucas.workshopmongodb.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource Not Found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> databaseError(DataBaseException e, HttpServletRequest request){
+		String error = "DataBase Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	
+}
--- src/main/java/com/joaolucas/workshopmongodb/services/UserService.java
+++ src/main/java/com/joaolucas/workshopmongodb/services/UserService.java
@@ -1,12 +1,17 @@
 package com.joaolucas.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.joaolucas.workshopmongodb.entities.User;
 import com.joaolucas.workshopmongodb.repositories.UserRepository;
+import com.joaolucas.workshopmongodb.services.exceptions.DataBaseException;
+import com.joaolucas.workshopmongodb.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +22,34 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(String id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+	}
+
+	/*
+	 * public User update(String id, User obj) { try { User entity =
+	 * repository.getReferenceById(id); updateData(entity, obj); return
+	 * repository.save(entity); } catch (EntityNotFoundException e) { throw new
+	 * ResourceNotFoundException(id); } }
+	 * 
+	 * private void updateData(User entity, User obj) {
+	 * entity.setName(obj.getName()); entity.setEmail(obj.getEmail()); }
+	 */
 
 }
