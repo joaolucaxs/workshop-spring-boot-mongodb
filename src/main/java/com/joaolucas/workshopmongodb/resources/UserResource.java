feca520fa0c5f8ddce819e0c1bd21278ec0aba17
João Lucas <joo.lucas994@gmail.com>
2023-03-09 18:14:45 -0300

    Deletando usuário por id

--- src/main/java/com/joaolucas/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/joaolucas/workshopmongodb/resources/UserResource.java
@@ -5,8 +5,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +37,33 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		return ResponseEntity.ok().body(obj);
+
+		/*
+		 * or URI uri =
+		 * ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand
+		 * (obj.getId()).toUri(); return ResponseEntity.created(uri).body(obj);
+		 */
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
+
+	/*
+	 * @PutMapping(value = "/{id}") public ResponseEntity<User> update(@PathVariable
+	 * String id, @RequestBody User obj) { obj = service.update(id, obj); return
+	 * ResponseEntity.ok().body(obj); }
+	 */
 
 
 
--- src/main/java/com/joaolucas/workshopmongodb/services/UserService.java
+++ src/main/java/com/joaolucas/workshopmongodb/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.joaolucas.workshopmongodb.dtos.UserDTO;
 import com.joaolucas.workshopmongodb.entities.User;
 import com.joaolucas.workshopmongodb.repositories.UserRepository;
 import com.joaolucas.workshopmongodb.services.exceptions.DataBaseException;
@@ -34,6 +35,7 @@ public class UserService {
 
 	public void delete(String id) {
 		try {
+			findById(id);
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
@@ -43,13 +45,17 @@ public class UserService {
 	}
 
 	/*
-	 * public User update(String id, User obj) { try { User entity =
-	 * repository.getReferenceById(id); updateData(entity, obj); return
+	 * public User update(String id, User obj) { try { Optional<User> entity =
+	 * repository.findById(id); updateData(entity, obj); return
 	 * repository.save(entity); } catch (EntityNotFoundException e) { throw new
 	 * ResourceNotFoundException(id); } }
 	 * 
-	 * private void updateData(User entity, User obj) {
+	 * private void updateData(Optional<User> entity, User obj) {
 	 * entity.setName(obj.getName()); entity.setEmail(obj.getEmail()); }
 	 */
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 
 }
