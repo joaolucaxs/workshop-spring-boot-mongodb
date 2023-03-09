083c7e297e3bcc99eecf4ad496f97a1449888598
João Lucas <joo.lucas994@gmail.com>
2023-03-09 18:58:55 -0300

    Obtendo post por id

---
+++ src/main/java/com/joaolucas/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.joaolucas.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaolucas.workshopmongodb.entities.Post;
+import com.joaolucas.workshopmongodb.repositories.PostRepository;
+import com.joaolucas.workshopmongodb.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+}
